036924b152fe99f26d5fd1c5cbffafbde954aebc
Ruslan Bairamoff <123728672+Ruslan061192@users.noreply.github.com>
2025-08-09 22:45:57 +0300

    delete key

--- WeatherApp/src/core/constants/axiosConstants.ts
+++ WeatherApp/src/core/constants/axiosConstants.ts
@@ -10,4 +10,3 @@ export const CHECK_USER_AUTH: string = "/users/is-available"
 export const CREATE_USER: string = "/users/"
 export const REFRESH_TOKEN_URL: string = "https://api.escuelajs.co/api/v1/auth/refresh-token"
 
-// b2454e4309ff4b37ac5122134253005
